0afd932be111329b11631be1841b14c44918dfd6
Sophie <tofuisshartingbecauseofthemala@gmail.com>
2024-07-20 16:11:34 +0800

    did one box for my plan

--- questionnaire/MyPlanView.swift
+++ questionnaire/MyPlanView.swift
@@ -9,9 +9,35 @@ import SwiftUI
 
 struct MyPlanView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+            Text("My plan (30 Days)")
+                .bold()
+                .font(.system(size: 36))
+        VStack (alignment: .leading, spacing: 40) {
+                            Text("Day 1")
+                                .bold()
+                                .italic()
+                                .underline()
+                                .foregroundStyle(.purple)
+                                .padding()
+                                .background(.white)
+                                .font(.largeTitle)
+                                .font(.system(size: 50))
+                            Text("15 pushups- 30 seconds")
+                            Text("1 minute plank")
+                            Text("30 squats- 1 minute")
+                            Text("Jumping jacks- 1 minute")
+                            Text("30 leg raises- 1 minute")
+            Text("30 sit ups- 1 minute 15 seconds")
+               
+                            }
+        .frame(width: 300)
+        .padding()
+        .background(.pink)
+        .cornerRadius(20)
+                        }
+                       
+                    }
+        
 
 #Preview {
     MyPlanView()
