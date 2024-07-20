eb9f337687f40133f16d050638ca110fdb59c133
avrilwl <alex410w3asl3y@gmail.com>
2024-07-20 13:36:56 +0800

    Initial Commit

---
+++ questionnaire/questionnaireApp.swift
@@ -0,0 +1,17 @@
+//
+//  questionnaireApp.swift
+//  questionnaire
+//
+//  Created by Li Jiansheng on 20/7/24.
+//
+
+import SwiftUI
+
+@main
+struct questionnaireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
