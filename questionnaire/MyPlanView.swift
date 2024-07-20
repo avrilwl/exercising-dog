3083005618e8bffb77c490b114abcabfbc837ee4
Sophie <tofuisshartingbecauseofthemala@gmail.com>
2024-07-20 16:51:07 +0800

    added the scroll view plan thingy two of them

--- questionnaire/MyPlanView.swift
+++ questionnaire/MyPlanView.swift
@@ -9,35 +9,58 @@ import SwiftUI
 
 struct MyPlanView: View {
     var body: some View {
-            Text("My plan (30 Days)")
-                .bold()
-                .font(.system(size: 36))
-        VStack (alignment: .leading, spacing: 40) {
-                            Text("Day 1")
-                                .bold()
-                                .italic()
-                                .underline()
-                                .foregroundStyle(.purple)
-                                .padding()
-                                .background(.white)
-                                .font(.largeTitle)
-                                .font(.system(size: 50))
-                            Text("15 pushups- 30 seconds")
-                            Text("1 minute plank")
-                            Text("30 squats- 1 minute")
-                            Text("Jumping jacks- 1 minute")
-                            Text("30 leg raises- 1 minute")
-            Text("30 sit ups- 1 minute 15 seconds")
-               
-                            }
-        .frame(width: 300)
-        .padding()
-        .background(.pink)
-        .cornerRadius(20)
-                        }
-                       
-                    }
+        Text("My plan (30 Days)")
+            .bold()
+            .font(.system(size: 36))
+        ScrollView{
+            VStack (alignment: .leading, spacing: 40) {
+                Text("Day 1")
+                    .bold()
+                    .italic()
+                    .underline()
+                    .foregroundStyle(.purple)
+                    .padding()
+                    .background(.white)
+                    .font(.largeTitle)
+                    .font(.system(size: 50))
+                Text("15 pushups- 30 seconds")
+                Text("1 minute plank")
+                Text("30 squats- 1 minute")
+                Text("Jumping jacks- 1 minute")
+                Text("30 leg raises- 1 minute")
+                Text("30 sit ups- 1 minute 15 seconds")
+                
+            }
+            .frame(width: 300)
+            .padding()
+            .background(.pink)
+            .cornerRadius(20)
+            VStack (alignment: .leading, spacing: 40) {
+                Text("Day 2")
+                    .bold()
+                    .italic()
+                    .underline()
+                    .foregroundStyle(.purple)
+                    .padding()
+                    .background(.white)
+                    .font(.largeTitle)
+                    .font(.system(size: 50))
+                Text("15 pushups- 30 seconds")
+                Text("1 minute plank")
+                Text("30 squats- 1 minute")
+                Text("Jumping jacks- 1 minute")
+                Text("30 leg raises- 1 minute")
+                Text("30 sit ups- 1 minute 15 seconds")
+                
+            }
+            .frame(width: 300)
+            .padding()
+            .background(.blue)
+            .cornerRadius(20)
+        }
         
+    }
+}
 
 #Preview {
     MyPlanView()
