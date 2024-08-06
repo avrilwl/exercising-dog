6d5a01252e12e3ee7a33304d5debfcf193d8d63b
avrilwl <alex410w3asl3y@gmail.com>
2024-08-06 22:50:46 +0800

    added buttons w/o actions

--- questionnaire/ChoosePlanView.swift
+++ questionnaire/ChoosePlanView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ChoosePlanView: View {
+    
+    
     var body: some View {
         ZStack {
             Color.blue
@@ -15,6 +17,49 @@ struct ChoosePlanView: View {
             VStack {
                 Text("Choose the duration of your plan!")
                     .font(.largeTitle)
+                
+                Button(action: {
+                    
+                }) {
+                    Text("7 day workout plan")
+                        .frame(width: 350, height: 50)
+                        .font(.system(size: 30))
+                        .foregroundColor(.black)
+                        .background(Color.green)
+                        .cornerRadius(15)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(Color.black, lineWidth: 3)
+                        )
+                }
+                Button(action: {
+                    
+                }) {
+                    Text("30 day workout plan")
+                        .frame(width: 350, height: 50)
+                        .font(.system(size: 30))
+                        .foregroundColor(.black)
+                        .background(Color.yellow)
+                        .cornerRadius(15)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(Color.black, lineWidth: 3)
+                        )
+                }
+                Button(action: {
+                    
+                }) {
+                    Text("100 day workout plan")
+                        .frame(width: 350, height: 50)
+                        .font(.system(size: 30))
+                        .foregroundColor(.black)
+                        .background(Color.red)
+                        .cornerRadius(15)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(Color.black, lineWidth: 3)
+                        )
+                }
             }
         }
     }
